43f174ad7179b82bf61edca692461b239282827b
e-fumiya <efumiya0906@gmail.com>
2025-12-17 03:16:28 +0900

    added the struct definitions for the lexer and parser

--- include/minishell.h
+++ include/minishell.h
@@ -3,17 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
+/*   By: fendo <fendo@student.42.jp>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 20:40:52 by tafujise          #+#    #+#             */
-/*   Updated: 2025/12/05 20:48:18 by tafujise         ###   ########.fr       */
+/*   Updated: 2025/12/17 03:07:11 by fendo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef enum e_op
+{
+	OP_AND_IF,
+	OP_OR_IF,
+	OP_LESS,
+	OP_GRAET,
+	OP_DLESS,
+	OP_DGREAT,
+	OP_PIPE,
+	OP_RPAREN,
+	OP_LPAREN,
+}	t_op;
+
+typedef enum e_flag
+{
+	W_SQ	= 1u << 0,
+	W_DQ	= 1u << 1,
+	W_DOLL	= 1u << 2,
+	W_WILD	= 1u << 3,
+}	t_flag;
+
+typedef struct s_word
+{
+	char	*str;
+	size_t	len;
+}	t_word;
+
+typedef struct s_word_desc
+{
+	t_word	word;
+	size_t	flag;
+}	t_word_desc;
+
 #endif
-
-
-
---
+++ include/parser.h
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fendo <fendo@student.42.jp>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 03:00:37 by fendo             #+#    #+#             */
+/*   Updated: 2025/12/17 03:15:39 by fendo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+# include "minishell.h"
+
+typedef struct s_redirect
+{
+	t_op		op;
+	t_word_desc	target; // filename or delimiter
+	int			fd;
+}	t_redirect;
+
+typedef struct s_simple_cmd
+{
+	t_word_desc	*assigns;
+	t_word_desc	*args;
+	t_redirect	*redirects;
+}	t_simple_cmd;
+
+typedef struct s_subshell
+{
+	t_program	*program;
+	t_redirect	*redirects;
+}	t_subshell;
+
+typedef enum e_cmd_kind
+{
+	CMD_SIMPLE,
+	CMD_SUBSHELL
+}	t_cmd_kind;
+
+typedef struct s_command
+{
+	t_cmd_kind	cmd_kind;
+	union
+	{
+		t_simple_cmd	simple;
+		t_subshell		subshell;
+	};
+}	t_command;
+
+typedef struct s_pipeline
+{
+	t_command	*cmds;
+}	t_pipeline;
+
+typedef struct s_and_or
+{
+	t_pipeline	*pipes;
+	t_op		*ops;
+}	t_and_or;
+
+typedef struct s_complete_commands
+{
+	t_and_or	*and_ors;
+}	t_complete_commands;
+
+typedef struct s_program
+{
+	t_complete_commands	comp_cmds;
+}	t_program;
+
+#endif
