6c499d86dbd6db0ed0b5edf21f6f92ed9f58a729
TaisukeFujise <tafujise3502@icloud.com>
2025-12-26 04:47:32 +0900

    fix: Fix the lack of checking return value of hash create func.

--- src/execute/init.c
+++ src/execute/init.c
@@ -6,7 +6,7 @@
 /*   By: tafujise <tafujise@student.42jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 20:37:12 by tafujise          #+#    #+#             */
-/*   Updated: 2025/12/25 22:52:24 by tafujise         ###   ########.fr       */
+/*   Updated: 2025/12/26 03:31:48 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	init_executor(t_exec *executor, char **envp)
 {
 	ft_bzero(executor, sizeof(t_exec));
 	executor->env_table = hash_create(BUCKET_SIZE);
+	if (executor->env_table == NULL)
 		return (FAILURE);
 	if (_load_envp(executor->env_table, envp) == FAILURE)
 		return (FAILURE);
@@ -27,7 +28,7 @@ int	init_executor(t_exec *executor, char **envp)
 
 static int	_load_envp(t_hashtable *env_table, char **envp)
 {
-	// TODO:w
+	// TODO
 	//
 	// hash function makes envp key to hash value, and then hash value divided by hash table size is index of hash table.
 	// hash table[index] has struct entry pointer.
