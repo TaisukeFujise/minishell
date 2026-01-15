da663fcadd448b3a596058c86c815364d48f8f2b
TaisukeFujise <tafujise3502@icloud.com>
2026-01-15 22:24:14 +0900

    Add TODO comment and format style in execute.c

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -6,21 +6,21 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 19:27:55 by tafujise          #+#    #+#             */
-/*   Updated: 2026/01/14 21:47:53 by tafujise         ###   ########.fr       */
+/*   Updated: 2026/01/15 22:23:30 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/parser.h"
 #include "../../include/execute.h"
+#include "../../include/parser.h"
 
 void	exec_complete(t_node *node, t_exec *executor)
 {
 	while (node != NULL)
 	{
 		execute(node->child, executor);
-		node = node->next;	
+		node = node->next;
 	}
-}	
+}
 
 void	exec_andor(t_node *node, t_exec *executor)
 {
@@ -28,14 +28,16 @@ void	exec_andor(t_node *node, t_exec *executor)
 	{
 		execute(node->child, executor);
 		if (node->u_node.and_or.op == CONNECT_AND_IF)
-			if (executor->exit_code != 0)// If the last exit status is not 0, end the process.
-				break; 
-		if (node->u_node.and_or.op == CONNECT_OR_IF)	
-			if (executor->exit_code == 0) // If the last exit status is 0, end the process.
-				break;		
-		node = node->next;	
-	}	
-}	
+			if (executor->exit_code != 0)
+				// If the last exit status is not 0,end the process.
+				break ;
+		if (node->u_node.and_or.op == CONNECT_OR_IF)
+			if (executor->exit_code == 0)
+				// If the last exit status is 0,end the process.
+				break ;
+		node = node->next;
+	}
+}
 //
 void	exec_pipe(t_node *node, t_exec *executor)
 {
@@ -45,44 +47,49 @@ void	exec_pipe(t_node *node, t_exec *executor)
 
 	while (node != NULL)
 	{
-		// TODO
-		// first and last simple cmds requires separate process
-		// other simple cmds has the same process
-		// also subshell may happens here 
+		/*
+			TODO
+			- First and last simple cmds requires separate process
+			- Other simple cmds has the same process
+			- also subshell may happens here.
+		*/
 		node = node->next;
 	}
-}	
+}
 
 void	exec_simple(t_node *node, t_exec *executor)
 {
-	// TODO
-	// First expand Environment Variable
-	// Second "built-in" or "execve" Using executor->input_fd and executor->output_fd
+	/*
+		TODO
+		- Create tmp_env_table from "t_word_list *assigns"
+		- Expand Environment Variable
+		- Set Redirect from "t_redirect *redirects"
+		- Execute cmd, which is "built-in" or "execve",
+			using executor->input_fd and executor->output_fd.
+	*/
 }
 
 void	exec_subshell(t_node *node, t_exec *executor)
 {
 	// TODO
 	// subshell
-}	
+}
 
 // Only dispatch exec_hogehoge by node_kind. (No recursive call of execute here)
 void	execute(t_node *node, t_exec *executor)
 {
 	if (node == NULL) // when user_input is empty, ast root node is NULL.
-		return ;  // it's NULL guard for thath
-	if (node->node_kind == NODE_COMPLETE) 
+		return ;       // it's NULL guard for thath
+	if (node->node_kind == NODE_COMPLETE)
 		exec_complete(node, executor);
 	else if (node->node_kind == NODE_ANDOR)
-		exec_andor(node, executor); 
+		exec_andor(node, executor);
 	else if (node->node_kind == NODE_PIPE) // 中でnext
 		exec_pipe(node, executor);
 	else if (node->node_kind == NODE_SUBSHELL)
 		exec_subshell(node, executor);
 	else if (node->node_kind == NODE_SIMPLE)
-		exec_simple(node, executor); 
+		exec_simple(node, executor);
 	else
-		exit(1);	
+		exit(1);
 }
-
-
