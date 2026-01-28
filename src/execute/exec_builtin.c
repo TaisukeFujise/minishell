0d78ff921976c930f161ee08a4a1b923741d0ca6
TaisukeFujise <tafujise3502@icloud.com>
2026-01-28 23:14:19 +0900

    feat: Add reset_redirects call in exec_builtin

--- src/execute/exec_builtin.c
+++ src/execute/exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 00:45:59 by tafujise          #+#    #+#             */
-/*   Updated: 2026/01/28 23:08:42 by tafujise         ###   ########.fr       */
+/*   Updated: 2026/01/28 23:13:41 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ t_status	exec_builtin(t_simple_cmd *cmd, t_ctx *ctx, int pipe_in, int pipe_out)
 {
 	t_status	result;
 	if (apply_redirects(cmd->redirects, ctx) == ST_FATAL)
+		return (ST_FATAL);
+	if (reset_redirects(cmd->redirects, ctx) == ST_FATAL)
 		return (ST_FATAL);
 	return (result);
 }
