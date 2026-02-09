766b9d14a91e948d84aa60e43b0995a1974058c6
TaisukeFujise <tafujise3502@icloud.com>
2026-02-09 23:37:18 +0900

    feat: Add comment about cd_cmd on the way

--- src/builtin/cd_cmd.c
+++ src/builtin/cd_cmd.c
@@ -6,7 +6,7 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 19:33:34 by tafujise          #+#    #+#             */
-/*   Updated: 2026/02/07 22:55:01 by tafujise         ###   ########.fr       */
+/*   Updated: 2026/02/09 23:37:10 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,15 @@
 	cd [directory]
 	"cd" change current working directory, and update the ctx->exit_code.
 	If no parameters, change the current working directory to $HOME.
+	- 1. no directory operand with no HOME environment
+	- 2. no directory operand with HOME environment
+	- 3. directory operand begins with a <slash> character
+	- 4. the first component of the directory operand is dot or dot-dot
+	<reference>
+	[ https://pubs.opengroup.org/onlinepubs/9699919799/utilities/cd.html ]
 */
 t_status	cd_cmd(t_word_list *args, t_ctx *ctx)
 {
+	(void)ctx;
+	
 }
