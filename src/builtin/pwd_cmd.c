f4d179b0cc2826a87611a020ab7fd046f5e1e80f
TaisukeFujise <tafujise3502@icloud.com>
2026-02-11 05:51:43 +0900

    feat: Add pwd_cmd

--- src/builtin/pwd_cmd.c
+++ src/builtin/pwd_cmd.c
@@ -6,7 +6,7 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 20:41:22 by tafujise          #+#    #+#             */
-/*   Updated: 2026/02/08 00:15:14 by tafujise         ###   ########.fr       */
+/*   Updated: 2026/02/11 02:20:18 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,17 @@
 
 /*
 	pwd
-	"pwd" display $PWD, and update the ctx->exit_code.
+	"pwd" invoke getcwd(), and update the ctx->exit_code.
+	It returns always physical path. It's different from "pwd" in bash.
 */
 t_status	pwd_cmd(t_word_list *args, t_ctx *ctx)
 {
+	char	*path;
+
+	if (args != NULL)
+		return (ST_FAILURE); // pwd: too many arguments.
+	path = getcwd(NULL, 0);
+	if (path == NULL)
+		return (ST_FAILURE); 
+	printf("$s\n", path);
 }
