b1f11c659422c687fc24e3f2746d84e387db624b
TaisukeFujise <tafujise3502@icloud.com>
2026-02-11 05:52:36 +0900

    feat: exit_cmd

--- src/builtin/exit_cmd.c
+++ src/builtin/exit_cmd.c
@@ -6,7 +6,7 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 20:40:04 by tafujise          #+#    #+#             */
-/*   Updated: 2026/02/08 00:05:29 by tafujise         ###   ########.fr       */
+/*   Updated: 2026/02/11 04:12:05 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,27 @@
 */
 t_status	exit_cmd(t_word_list *args, t_ctx *ctx)
 {
+	long	arg_num;
+
+	if (args == NULL)
+	{
+		ctx->err.exit_code = 0;
+		return (ST_EXIT);
+	}
+	if (count_args(args) > 2)
+	{
+		write(STDERR_FILENO, "exit", 4);
+		ctx->err.exit_code = 1;
+		return (ST_FAILURE); // exit: too many arguments
+	}
+	arg_num = ft_strtol(args->wd->str);
+	if (errno == ERANGE)
+	{
+		write(STDERR_FILENO, "exit", 4);
+		ctx->err.exit_code = 2;
+		return (ST_FATAL); // exit: (arg->wd->str): numeric argument reguired
+	}
+	write(STDOUT_FILENO, "exit", 4);
+	ctx->err.exit_code = arg_num % 256;
+	return (ST_EXIT);
 }
