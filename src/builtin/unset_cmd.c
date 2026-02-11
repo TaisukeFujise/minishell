ab8abbf5bf5f05748585c3678649b096e50b75c4
TaisukeFujise <tafujise3502@icloud.com>
2026-02-11 12:55:53 +0900

    feat: Add unset_cmd

--- src/builtin/unset_cmd.c
+++ src/builtin/unset_cmd.c
@@ -6,12 +6,12 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 20:41:53 by tafujise          #+#    #+#             */
-/*   Updated: 2026/02/08 00:10:34 by tafujise         ###   ########.fr       */
+/*   Updated: 2026/02/11 12:54:23 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/parser.h"
 #include "../../include/builtin.h"
+#include "../../include/parser.h"
 
 /*
 	unset name...
@@ -20,4 +20,28 @@
 */
 t_status	unset_cmd(t_word_list *args, t_ctx *ctx)
 {
+	int					i;
+	t_bucket_contents	*item;
+
+	i = 0;
+	if (args == NULL)
+		return (ST_FAILURE); // unset: not enough arguments
+	if (args->wd->len < 1)
+		return (ST_FAILURE);
+	while (args->wd->str[i])
+	{
+		if (!(ft_isalnum(args->wd->str[i]) || args->wd->str[i] == '_'))
+			return (ST_FAILURE);
+				// unset: (args->wd->str): invalid parameter name
+		i++;
+	}
+	if (ft_isdigit(*(args->wd->str)))
+		return (ST_FAILURE);
+	while (args)
+	{
+		item = hash_remove(args->wd->str, ctx->env_table);
+		free(item);
+		item = NULL;
+		args = args->next;
+	}
 }
