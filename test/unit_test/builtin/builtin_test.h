9fc4b0da2b784c885897176a894fac2d9f4a11e1
TaisukeFujise <tafujise3502@icloud.com>
2026-02-11 15:18:00 +0900

    feat: draft about builtin unit test

---
+++ test/unit_test/builtin/builtin_test.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_test.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 14:31:18 by tafujise          #+#    #+#             */
+/*   Updated: 2026/02/11 14:38:17 by tafujise         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTIN_TEST_H
+#define BUILTIN_TEST_H
+
+#include "../../../include/minishell.h"
+#include "../../../include/parser.h"
+#include "../../../include/execute.h"
+#include "../../../include/builtin.h"
+
+#include <assert.h>
+
+#endif BUILTIN_TEST_H
---
+++ test/unit_test/builtin/main.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 14:31:54 by tafujise          #+#    #+#             */
+/*   Updated: 2026/02/11 15:17:08 by tafujise         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtin_test.h"
+#include <stdio.h>
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_ctx				ctx;
+	t_word_list			*args;
+	t_word_list			*flag_args;
+	t_bucket_contents	*item;
+
+	/*
+		Test case
+		(i)   cd
+		(ii)  echo
+		(iii) env
+		(iv)  exit
+		(v)   export
+		(vi)  pwd
+		(vii) unset
+	*/
+	(void)argc;
+	(void)argv;
+	if (init_ctx(&ctx, envp) == FAILURE)
+		return (1);
+	// (i) cd
+	write(1, "==== cd ====", 12);
+	args = NULL;
+	cd_cmd(args, &ctx);
+	assert(ft_strcmp(getcwd(NULL, 0), "/home/tafujise/Cursus/minishell") == 0);
+	item = hash_search("PWD", ctx.env_table);
+	assert(ft_strcmp(item->data.value, "/home/tafujise/Cursus/minishell") == 0);
+	// item = hash_search("OLDPWD", ctx.env_table);
+	// assert(ft_strcmp(item->data.value,"/home/tafujise/Cursus/minishell") == 0);
+	args = malloc(sizeof(t_word_list));
+	args->wd->str = ft_strdup("src");
+	args->wd->len = 3;
+	args->next = NULL;
+	cd_cmd(args, &ctx);
+	assert(ft_strcmp(getcwd(NULL, 0),
+			"/home/tafujise/Cursus/minishell/src") == 0);
+	item = hash_search("PWD", ctx.env_table);
+	assert(ft_strcmp(item->data.value, "/home/tafujise/Cursus/minishell") == 0);
+	item = hash_search("OLDPWD", ctx.env_table);
+	assert(ft_strcmp(item->data.value, "/home/tafujise/Cursus") == 0);
+	// (ii) echo
+	write(1, "==== echo ====", 14);
+	args = NULL;
+	echo_cmd(args, &ctx);
+	args = malloc(sizeof(t_word_list));
+	args->wd->str = ft_strdup("abc");
+	args->wd->len = 3;
+	args->wd->next = NULL;
+	echo_cmd(args, &ctx);
+	flag_args = malloc(sizeof(t_word_list));
+	flag_args->wd->str = ft_strdup("-n");
+	flag_args->wd->len = 2;
+	flag_args->wd->next = args;
+	echo_cmd(flag_args, &ctx);
+	// (iii) env
+	write(1, "==== env ====", 13);
+	args = NULL;
+	env_cmd(args, &ctx);
+	// (iv) exit
+	// write(1, "==== exit ====", 14);
+	// (v) export
+	// write(1, "==== export ====", 16);
+	// (vi) pwd
+	write(1, "==== pwd ====", 13);
+	args = NULL;
+	pwd_cmd(args, &ctx);
+	// (vii) unset
+	write(1, "==== unset ====", 15);
+	args = NULL;
+	unset(args, &ctx);
+	args = malloc(sizeof(t_word_list));
+	args->wd->str = ft_strdup("HOME");
+	args->wd->len = 4;
+	args->next = NULL;
+	unset(args, &ctx);
+}
