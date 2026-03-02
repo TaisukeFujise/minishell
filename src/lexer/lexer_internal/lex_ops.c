a1fee5f3dd772e0fdf41fead0f14046f58f10f14
e-fumiya <efumiya0906@gmail.com>
2026-03-02 23:44:49 +0900

    modify lexer to extracte tokens one by one

--- src/lexer/lexer_internal/lex_ops.c
+++ src/lexer/lexer_internal/lex_ops.c
@@ -6,13 +6,20 @@
 /*   By: fendo <fendo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 19:59:19 by fendo             #+#    #+#             */
-/*   Updated: 2026/02/09 20:22:22 by fendo            ###   ########.fr       */
+/*   Updated: 2026/03/02 23:42:17 by fendo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../../../include/minishell.h"
 #include "lexer_internal.h"
 
+/*
+	parse.y ref:
+	- read_token: '\n' return (L3657), yacc_EOF (L3618-L3620)
+	lex rule:
+	- <INITIAL> "\0" -> TK_EOF  (paren_depth != 0 -> TK_ERR)
+	- <INITIAL> "\n" -> TK_NEWLINE
+*/
 t_token_kind	lex_control(char **line, t_token *token)
 {
 	if (**line == '\0')
@@ -20,12 +27,19 @@ t_token_kind	lex_control(char **line, t_token *token)
 	else if (**line == '\n')
 	{
 		token->token_kind = TK_NEWLINE;
-		token->u_token.nl_ptr = *line;
 		(*line)++;
 	}
 	return (token->token_kind);
 }
 
+/*
+	parse.y ref:
+	- read_token: AND_AND/OR_OR dispatch (L3715-L3723), '|' (L3814)
+	lex rule:
+	- <INITIAL> "&&" -> TK_CONNECT(AND_IF)
+	- <INITIAL> "||" -> TK_CONNECT(OR_IF)
+	- <INITIAL> "|"  -> TK_CONNECT(PIPE)
+*/
 t_token_kind	lex_connect(char **line, t_token *token)
 {
 	if (ft_strncmp(*line, "&&", 2) == 0)
@@ -49,6 +63,13 @@ t_token_kind	lex_connect(char **line, t_token *token)
 	return (token->token_kind);
 }
 
+/*
+	parse.y ref:
+	- read_token: '(' / ')' handling (L3781-L3810)
+	lex rule:
+	- <INITIAL> "(" -> TK_GROUP(LPAREN)   paren_depth++
+	- <INITIAL> ")" -> TK_GROUP(RPAREN)   paren_depth-- (>0)
+*/
 t_token_kind	lex_group(char **line, t_token *token, t_lex_state *st)
 {
 	if (**line == '(')
@@ -60,19 +81,25 @@ t_token_kind	lex_group(char **line, t_token *token, t_lex_state *st)
 	}
 	else if (**line == ')')
 	{
-		if (st->paren_depth == 0)
-			set_lexer_error(token, LEX_ERR_UNCLOSED_SUBSHELL);
-		else
-		{
-			token->token_kind = TK_GROUP;
-			token->u_token.op_group = GROUP_RPAREN;
+		token->token_kind = TK_GROUP;
+		token->u_token.op_group = GROUP_RPAREN;
+		if (st->paren_depth > 0)
 			st->paren_depth--;
-		}
 		(*line)++;
 	}
 	return (token->token_kind);
 }
 
+/*
+	parse.y ref:
+	- read_token: LESS_LESS/GREATER_GREATER (L3697-L3713),
+	  single '<' / '>' (L3725-L3762)
+	lex rule:
+	- <INITIAL> "<<" -> TK_REDIR(DLESS)
+	- <INITIAL> ">>" -> TK_REDIR(DGREATER)
+	- <INITIAL> "<"  -> TK_REDIR(LESS)
+	- <INITIAL> ">"  -> TK_REDIR(GREATER)
+*/
 t_token_kind	lex_redirect(char **line, t_token *token)
 {
 	if (ft_strncmp(*line, ">>", 2) == 0)
@@ -102,6 +129,12 @@ t_token_kind	lex_redirect(char **line, t_token *token)
 	return (token->token_kind);
 }
 
+/*
+	parse.y ref:
+	- read_token_word: all_digit_token + NUMBER (L5731-L5741)
+	lex rule:
+	- <INITIAL> DIGIT+/[<>] -> TK_IO_NUMBER (value <= INT_MAX)
+*/
 t_token_kind	lex_io_number(char **line, t_token *token)
 {
 	char			*end_ptr;
--- src/lexer/lexer_internal/lex_word.c
+++ src/lexer/lexer_internal/lex_word.c
@@ -6,53 +6,81 @@
 /*   By: fendo <fendo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 21:43:37 by fendo             #+#    #+#             */
-/*   Updated: 2026/02/12 00:23:02 by fendo            ###   ########.fr       */
+/*   Updated: 2026/03/02 23:43:15 by fendo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer_internal.h"
-#include "minishell.h"
+#include "../../../include/minishell.h"
 
-static t_lexer_err	scan_sq(char **line, t_word ***tail)
+/*
+	parse.y ref:
+	- read_token_word: single quote scanning (L5385-L5415)
+	lex rule:
+	- <SQ> [^']+ -> part(W_SQ)
+	- <SQ> "'"   -> end quote, return to <WORD>
+	- <SQ> "\0"  -> TK_ERR(UNCLOSED_SINGLE_QUOTE)
+*/
+static t_lexer_err	scan_sq(char **line, t_word_builder *wb)
 {
 	char	*begin;
 
 	begin = ++(*line);
-	while (**line && **line != '\'')
-		(*line)++;
-	return (finish_quote(line, tail, begin, W_SQ));
+	ft_strspn(line, "\'", false);
+	if (**line != '\'')
+		return (LEX_ERR_UNCLOSED_SINGLE_QUOTE);
+	return (close_quote_part(wb, line, begin, W_SQ));
 }
 
-static t_lexer_err	scan_dq(char **line, t_word ***tail)
+/*
+	parse.y ref:
+	- read_token_word: double quote scanning (L5417-L5503)
+	lex rule:
+	- <DQ> \$NAME   -> part(W_DQ|W_DOLL)
+	- <DQ> \$\?       -> part(W_DQ|W_DOLL)
+	- <DQ> \$         -> part(W_DQ)  (bare dollar)
+	- <DQ> [^"$\0]+   -> part(W_DQ)
+	- <DQ> '"'        -> end quote, return to <WORD>
+	- <DQ> "\0"       -> TK_ERR(UNCLOSED_DOUBLE_QUOTE)
+*/
+static t_lexer_err	scan_dq(char **line, t_word_builder *wb)
 {
 	char	*begin;
-	char	*dollar;
 	uint8_t	flag;
+	uint8_t	dollar_flag;
 
 	begin = ++(*line);
 	while (**line && **line != '\"')
 	{
+		flag = W_DQ;
 		if (**line == '$')
 		{
-			if (append_part(tail, begin, *line - begin, W_DQ)
-				!= LEX_NO_ERR)
-				return (LEX_ERR_MEMORY_ALLOCATION);
-			dollar = *line;
-			flag = W_NONE;
-			lex_dollar(line, &flag);
-			if (append_part(tail, dollar, *line - dollar, W_DQ | flag)
-				!= LEX_NO_ERR)
-				return (LEX_ERR_MEMORY_ALLOCATION);
-			begin = *line;
+			dollar_flag = W_NONE;
+			lex_dollar(line, &dollar_flag);
+			flag |= dollar_flag;
 		}
 		else
-			(*line)++;
+			ft_strspn(line, "\"$", false);
+		if (commit_part(wb, &begin, *line, flag) != LEX_NO_ERR)
+			return (LEX_ERR_MEMORY_ALLOCATION);
 	}
-	return (finish_quote(line, tail, begin, W_DQ));
+	if (**line != '\"')
+		return (LEX_ERR_UNCLOSED_DOUBLE_QUOTE);
+	return (close_quote_part(wb, line, begin, W_DQ));
 }
 
-static t_lexer_err	scan_unquoted(char **line, t_word ***tail,
-						t_assign_info *as)
+/*
+	parse.y ref:
+	- read_token_word: '$' handling (L5507-L5567), default char (L5700+)
+	lex rule:
+	- <WORD> \$NAME  -> part(W_DOLL)
+	- <WORD> \$\?      -> part(W_DOLL)
+	- <WORD> \$        -> part(W_NONE)
+	- <WORD> \*+       -> part(W_WILD)
+	- <WORD> PLAIN+  -> part(W_NONE)
+*/
+static t_lexer_err	scan_unquoted(char **line, t_word_builder *wb,
+									t_assign_info *as)
 {
 	char	*begin;
 	uint8_t	flag;
@@ -64,40 +92,53 @@ static t_lexer_err	scan_unquoted(char **line, t_word ***tail,
 	else if (**line == '*')
 	{
 		flag = W_WILD;
-		while (**line == '*')
-			(*line)++;
+		ft_strspn(line, "*", true);
 	}
 	else
 		while (**line && !is_tk_bound(*line) && !ft_strchr("\'\"$*", **line))
 			validate_assign((*line)++, as);
-	if (append_part(tail, begin, *line - begin, flag) != LEX_NO_ERR)
-		return (LEX_ERR_MEMORY_ALLOCATION);
-	return (LEX_NO_ERR);
+	return (commit_part(wb, &begin, *line, flag));
 }
 
-static t_lexer_err	scan_word(char **line, t_word **head, t_assign_info *as)
+/*
+	parse.y ref:
+	- read_token_word: main loop (L5345-L5700)
+	lex rule:
+	- <WORD> dispatch: scan_sq / scan_dq / scan_unquoted
+	- <WORD> BOUND|DBOUND|\0 -> emit TK_WORD, BEGIN(INITIAL)
+*/
+static t_lexer_err	scan_word(char **line, t_word **head,
+								t_assign_info *as, t_arena *arena)
 {
-	t_word		**tail;
-	t_lexer_err	err;
+	t_word_builder	wb;
+	t_lexer_err		err;
 
-	tail = head;
+	wb.arena = arena;
+	wb.tail = head;
 	while (**line && !is_tk_bound(*line))
 	{
 		if (ft_strchr("\'\"$*", **line))
 			validate_assign(*line, as);
 		if (**line == '\'')
-			err = scan_sq(line, &tail);
+			err = scan_sq(line, &wb);
 		else if (**line == '\"')
-			err = scan_dq(line, &tail);
+			err = scan_dq(line, &wb);
 		else
-			err = scan_unquoted(line, &tail, as);
+			err = scan_unquoted(line, &wb, as);
 		if (err)
 			return (err);
 	}
 	return (LEX_NO_ERR);
 }
 
-t_token_kind	lex_word(char **line, t_token *tk)
+/*
+	parse.y ref:
+	- read_token_word: entry + got_token (L5305, L5720-L5741)
+	lex rule:
+	- <INITIAL> -> BEGIN(WORD) -> emit TK_WORD
+	- apply_assign_info: Apply the result of side-channel FSM
+*/
+t_token_kind	lex_word(char **line, t_token *tk, t_arena *arena)
 {
 	t_word			*head;
 	t_assign_info	as;
@@ -105,10 +146,9 @@ t_token_kind	lex_word(char **line, t_token *tk)
 
 	head = NULL;
 	set_assign_info(&as, AS_INIT, NULL, W_NONE);
-	err = scan_word(line, &head, &as);
+	err = scan_word(line, &head, &as, arena);
 	if (err)
 	{
-		free_word_parts(head);
 		if (err != LEX_ERR_MEMORY_ALLOCATION)
 			set_lexer_error(tk, err);
 		return (tk->token_kind);
