35b76edc026062f0d7d3f79b69da5bcfd84a959d
TaisukeFujise <tafujise3502@icloud.com>
2026-04-19 22:38:48 +0900

    fix: add result == ST_EXIT condition

--- src/execute/dispatch/exec_connection.c
+++ src/execute/dispatch/exec_connection.c
@@ -6,7 +6,7 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 00:09:17 by tafujise          #+#    #+#             */
-/*   Updated: 2026/02/11 11:03:42 by tafujise         ###   ########.fr       */
+/*   Updated: 2026/04/19 22:38:15 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,14 @@ t_status	exec_connection(t_node *node, t_ctx *ctx, int pipe_in, int pipe_out)
 
 t_status	exec_complete(t_node *node, t_ctx *ctx, int pipe_in, int pipe_out)
 {
-	// while (node != NULL)
-	// {
-	// 	result = execute(node->child, executor, ctx);
-	// 	if (result == ST_FATAL)
-	// 		return (ST_FATAL);
-	// 	node = node->next;
-	// }
-	// return (result);
+	t_status	result;
+
+	result = execute_internal(node->left, ctx, pipe_in, pipe_out);
+	if (result == ST_EXIT || result == ST_FATAL)
+		return (result);
+	if (node->right == NULL)
+		return (result);
+	return (execute_internal(node->right, ctx, pipe_in, pipe_out));
 }
 
 t_status	exec_andor(t_node *node, t_ctx *ctx, int pipe_in, int pipe_out)
