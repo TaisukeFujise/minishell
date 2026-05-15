fc99232c4d08b5a9ae06be74ab392f5fd8db947b
e-fumiya <efumiya0906@gmail.com>
2026-05-15 20:41:05 +0900

    fix: modify ft_strjoin_free

--- libft/ft_strjoin_free.c
+++ libft/ft_strjoin_free.c
@@ -2,6 +2,11 @@
 
 static void	free_strs(char *s1, char *s2, int flags)
 {
+	if (s1 && s1 == s2 && (flags & (1 << 0)) && (flags & (1 << 1)))
+	{
+		free(s1);
+		return ;
+	}
 	if ((flags & (1 << 0)) && s1)
 		free(s1);
 	if ((flags & (1 << 1)) && s2)
@@ -18,9 +23,17 @@ char	*ft_strjoin_free(char *s1, char *s2, int flags)
 		return (free_strs(s1, s2, flags), NULL);
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
+	if (len1 > SIZE_MAX - len2 - 1)
+	{
+		free_strs(s1, s2, flags);
+		return (NULL);
+	}
 	ans = malloc(len1 + len2 + 1);
 	if (!ans)
-		return (free_strs(s1, s2, flags), NULL);
+	{
+		free_strs(s1, s2, flags);
+		return (NULL);
+	}
 	ft_memcpy(ans, s1, len1);
 	ft_memcpy(ans + len1, s2, len2 + 1);
 	free_strs(s1, s2, flags);
