ec4ebd87017ec11a505d7dc425344d24d25fcfed
e-fumiya <efumiya0906@gmail.com>
2026-08-15 00:11:36 +0900

    perf(expand): avoid repeated strlen in heredoc expansion

--- src/expand/expand_internal/expand_heredoc.c
+++ src/expand/expand_internal/expand_heredoc.c
@@ -1,12 +1,12 @@
 #include "expand_internal.h"
 
-static bool	expand_dollar(t_strbuf *buf, t_ctx *ctx, char **body)
+static bool	add_dollar(t_strbuf *buf, t_ctx *ctx, t_word *raw, char **body)
 {
 	char	*value;
 	t_param	param;
 
 	param.s = *body;
-	param.slen = ft_strlen(*body);
+	param.slen = raw->len - (*body - raw->str);
 	value = expand_param(ctx, buf->arena, &param);
 	if (!value || !strbuf_add(buf, value, param.len))
 		return (false);
@@ -31,11 +31,11 @@ t_status	expand_heredoc_body(t_redirect *redir, t_ctx *ctx, t_arena *arena)
 	{
 		next = ft_strchr(body, '$');
 		if (!next)
-			next = body + ft_strlen(body);
+			next = redir->hd.raw_str.str + redir->hd.raw_str.len;
 		if (!strbuf_add(&buf, body, next - body))
 			return (ST_FATAL);
 		body = next;
-		if (*body && !expand_dollar(&buf, ctx, &body))
+		if (*body && !add_dollar(&buf, ctx, &redir->hd.raw_str, &body))
 			return (ST_FATAL);
 	}
 	redir->hd.raw_str.str = buf.data;
